77893450aaf8168b31191cbcc1cd9beaad5a0c8e
Faruk Colak <faruk_colak53@hotmail.com>
2021-02-15 13:06:20 +0300

    sprites added.

--- core/src/com/flappybird/game/FlappyBird.java
+++ core/src/com/flappybird/game/FlappyBird.java
@@ -1,31 +1,53 @@
 package com.flappybird.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture background;
+    Texture[] birds;
+    int birdState = 0;
+    int pause = 0;
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        birds = new Texture[2];
+        birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
+
+    }
+
+    @Override
+    public void render() {
+
+        if (pause < 8){
+            pause++;
+        }
+        else {
+            pause = 0;
+            if (birdState == 0) {
+                birdState = 1;
+            }
+            else {
+                birdState = 0;
+            }
+        }
+
+        batch.begin();
+
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(birds[birdState], Gdx.graphics.getWidth()/2 - birds[birdState].getWidth()/2, Gdx.graphics.getHeight()/2 - birds[birdState].getHeight()/2);
+
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
